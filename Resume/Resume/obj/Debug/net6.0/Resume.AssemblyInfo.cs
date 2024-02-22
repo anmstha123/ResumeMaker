71057efe0e098cb1804d4753d3b3db79f8292200
DIBESHPC\broad <dibeshrana.dr@gmail.com>
2024-02-22 11:33:25 -0600

    Education form working. formatted the home page >> error on the second home page button

--- Resume/Resume/obj/Debug/net6.0/Resume.AssemblyInfo.cs
+++ Resume/Resume/obj/Debug/net6.0/Resume.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccafa0906654f8028d014f29d13a37a1a5a9de3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
